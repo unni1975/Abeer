608196e977bc075bdf54ea11ebc92729b8f8c176
Unni <k.s.unni@gmail.com>
2018-05-01 11:00:39 +0530

    browservc

---
+++ CMMedAppointment/BrowserViewController.swift
@@ -0,0 +1,39 @@
+//
+//  BrowserViewController.swift
+//  CMMedAppointment
+//
+//  Created by Unni K S on 4/30/18.
+//  Copyright © 2018 calmove. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class BrowserViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    var urlString:String!
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if((urlString != nil) && urlString.count > 0) {
+            let myURL = URL(string: urlString)
+            let myRequest = URLRequest(url: myURL!)
+            webView.load(myRequest)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("Loading")
+    }
+    
+}
+
